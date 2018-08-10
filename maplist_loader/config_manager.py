78faf48ed13b42c37bdfec3f47c51c7b82a11902
Rush_FTK <867871766@qq.com>
2018-08-10 18:24:18 +0800

    update config_manager: can read/load config.json now

--- maplist_loader/config_manager.py
+++ maplist_loader/config_manager.py
@@ -3,15 +3,58 @@
 import sys,os
 import json
 
-class config_manager():
+class Config_Manager():
     def __init__(self):
         pass
 
-    def init_config(self):
+    def init_config(Config_Manager):
         "在当前路径下创建一个包含必要参数的空白的cofing文件"
-        cur_path = os.path.realpath(__file__)
+        initdata = {
+            'last_path' : '',
+        }
+        if (not Config_Manager.save_config(initdata)):
+            return 0
+        else:
+            return 1
+
+    def save_config(Config_Manager,config_data):
+        "将json字符串存储到文件中"
+        try:
+            with open('config.json','w') as f:
+                json.dump(config_data, f , sort_keys=True, indent=4, separators=(',', ': '))
+        except Exception as e:
+            print(e)
+            return 1
+        return 0
+
+    def load_config(Config_Manager):
+        "从config.json中读取信息"
+        try:
+            with open('config.json', 'r') as f:
+                data = json.load(f)
+        except Exception as e:
+            print(e)
+            return None
+        return data
+
+    def get_lastpath(Config_Manager):
+        "从config.json中读取lastpath字段"
+        data = Config_Manager.load_config()
+        if (data == None):
+            return None
+        else:
+            return data['last_path']
+
+    def save_lastpath(Config_Manager,new_lastpath):
+        "在其他变量不变的情况下，更新lastpath字段"
+        data = Config_Manager.load_config()
+        if (data == None):
+            return 1,"config.json不存在"
+        else:
+            data['last_path'] = new_lastpath
+            result = Config_Manager.save_config(data)
+        return result
 
 
 if __name__ == "__main__":
-    test_object = config_manager()
-    test_object.init_config()
+    pass
